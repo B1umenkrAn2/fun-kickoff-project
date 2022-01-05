4900fd3e3c20caf1b165f98a0e1aae000e848da3
HaloZod <william.siyang@gmail.com>
2022-01-04 23:48:33 -0500

    finished logout test

--- server/test/ping.spec.js
+++ server/test/ping.spec.js
@@ -7,6 +7,22 @@ chai.use(chaiHttp);
 
 console.log("test start")
 
+describe("/POST ping", () => {
+    it("it should return 200 and message", (done) => {
+        chai
+            .request(app)
+            .post(`/ping/`)
+            .send({message: "hello"})
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.have
+                    .property("response")
+                    .eql("Server is running. Message received: hello");
+                done();
+            });
+    });
+});
+
 
 describe("/ ping", () => {
     it("it should return 200 and a string", (done) => {
@@ -20,3 +36,28 @@ describe("/ ping", () => {
             });
     });
 });
+
+describe("/ login", () => {
+    it("it should return 200 and a success json with userdata  ", (done) => {
+        chai.request(server)
+            .post('/auth/login')
+            .send({email: "qq@qq.com", password: "Qwer1234"})
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object')
+                res.body.should.have.property('success')
+                chai.expect(res.body.success.user.email).to.eql('qq@qq.com')
+                res.should.have.cookie("token")
+                chai.request(server)
+                    .post('/auth/logout')
+                    .send({})
+                    .end((err,res)=>{
+                        res.should.have.status(200);
+                        res.text.should.eql("You have successfully logged out")
+                        chai.expect(res).to.not.have.cookie("token")
+                    })
+
+                done()
+            })
+    })
+})
